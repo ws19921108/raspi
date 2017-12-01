be530b3e3fbe18b4701445926f235c6b247e1a4c
ws19921108 <32241390+ws19921108@users.noreply.github.com>
2017-12-01 08:56:55 +0800

    1201 0856

---
+++ tuling.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+postdata = {
+    "key": "4085eeed768a4ec1848398e339f8f8bb",
+    "info": "你在哪里",
+    "loc":"广东省深圳市",
+    "userid":"123456"
+}
+
+def tuling(text):
+    postdata['info'] = text
+    r = requests.post("http://www.tuling123.com/openapi/api",data=postdata)
+    res = json.loads(r.text)['text']
+    return res
+
+print(tuling('说什么'))
