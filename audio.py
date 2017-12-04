fb8034cf5e8adc248087c640b44775e2921c6912
ws19921108 <ws19921108@163.com>
2017-12-04 22:16:11 +0800

    raspi

--- audio.py
+++ audio.py
@@ -1,64 +1,63 @@
-from time import sleep
+import threading
 import pygame
 import pyaudio
 import wave
-
-file='audio.mp3'
-pygame.mixer.init()
-print("播放音乐1")
-track = pygame.mixer.music.load(file)
-
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy():
-    pass
-pygame.mixer.music.stop()
-
-# instantiate PyAudio (1)
-p = pyaudio.PyAudio()
-CHUNK = 1024
+import time
+CHUNK = 4096
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 16000
-TIME = 5
+record_flag = False
 
-# define callback (2)
-# def callback(in_data, frame_count, time_info, status):
-#     data = stream.read(CHUNK)
-#     frames.append(in_data)
-#     return (in_data, pyaudio.paContinue)
 
-# open stream using callback (3)
-stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                frames_per_buffer=CHUNK)
-                # stream_callback=callback)
+def playMp3(filename):
+    pygame.mixer.init()
+    track = pygame.mixer.music.load(filename)
 
-# start the stream (4)
-stream.start_stream()
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        pass
+    pygame.mixer.music.stop()
+    
+def record():
+    global record_flag
 
-frames = []
-print(int(RATE / CHUNK * TIME))
-for i in range(0, int(RATE / CHUNK * TIME)):
-    print('.')
-    data = stream.read(CHUNK)
-    frames.append(data)
+    p = pyaudio.PyAudio()
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    output=False,
+                    frames_per_buffer=CHUNK)
 
-# # wait for stream to finish (5)
-# while TIME:
-#     sleep(1)
-#     TIME -= 1
+    stream.start_stream()
+    print('recording...')
+    frames = []
+    while record_flag:
+        data = stream.read(CHUNK)
+        frames.append(data)
 
-# stop stream (6)
-stream.stop_stream()
-stream.close()
-p.terminate()
-wf = wave.open('audio.wav', 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    print('end')
+    wf = wave.open('test.wav', 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
 
-# close PyAudio (7)
+def startRecord():
+    global record_flag
+    record_thd = threading.Thread(target=record)
+    record_thd.setDaemon(False)
+    record_thd.start()
+    record_flag= True    
+
+def stopRecord():
+    global record_flag
+    record_flag= False
+
+
+
